f1554b49575e29499c04009fd9d63fd850533948
Muhammad Ammar Tanweer <49334070+MUHAMMAD-AMMAR-TANWEER@users.noreply.github.com>
2021-10-03 19:13:01 +0500

    Added a post request api for minners and now it has two apis

--- app/index.js
+++ app/index.js
@@ -1,15 +1,28 @@
 const express  = require('express');
 const Blockchain = require('../blockchain');
+const bodyParser = require('body-parser');
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;//it allows to run on 3001
 //but when multiple instance run we use env variable
 
 const app = express();
+
+app.use(bodyParser.json());
 const bc = new Blockchain();
 //get app
 
 app.get('/blocks' , (req, res) => {
     res.json(bc.chain);
+});
+
+
+//post request
+
+app.post('/mine',(req,res) => {
+    const block = bc.addBlock(req.body.data);
+
+    console.log(`New block has been added: ${block.toString()} `);
+    res.redirect('/blocks');
 });
 
 app.listen(HTTP_PORT, () => console.log(`listening on port ${HTTP_PORT}`));
