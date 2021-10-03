85511092fd4eac2b0b499fa607c14d89fbd515d1
Muhammad Ammar Tanweer <49334070+MUHAMMAD-AMMAR-TANWEER@users.noreply.github.com>
2021-10-03 16:21:56 +0500

    created three test in blockchain test
    now all 7 tests are passed in jest

--- blockchain.test.js
+++ blockchain.test.js
@@ -25,6 +25,17 @@ describe ('Blockchain',() => {
         bc2.addBlock("foo");
         expect(bc.isValidChain(bc2.chain)).toBe(true);
     });
-
     
+    it("invalidates with corrupt genesis block data" , () => {
+        bc2.chain[0].data = "Bad data";
+        expect(bc.isValidChain(bc2.chain)).toBe(false);
+    });
+
+    it("invalidates the corrupt chain " , () => {
+        bc2.addBlock("foo");
+        bc2.chain[1].data = "Not foo";
+        expect(bc.isValidChain(bc2.chain)).toBe(false);
+    });
+
+     
 });
